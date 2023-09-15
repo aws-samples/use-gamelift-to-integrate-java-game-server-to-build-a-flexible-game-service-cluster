98c5eb9abbd9830fd994d194b6df4ce5ecd39132
dahuahua <38651850@qq.com>
2023-09-15 15:37:59 +0800

    add server sdk version

--- build-and-deploy-game-server.sh
+++ build-and-deploy-game-server.sh
@@ -6,4 +6,10 @@ cp ./build/libs/gamelift-java-game-server-demo-1.0-SNAPSHOT-all.jar ./ServerBuil
 region=us-east-1
 echo "Deploying build to GameLift in $region"
 buildversion=$(date +%Y-%m-%d.%H:%M:%S)
-aws gamelift upload-build --operating-system AMAZON_LINUX_2 --build-root ./ServerBuild/ --name "Java Game Server Example" --build-version $buildversion --region $region
+aws gamelift upload-build \
+    --operating-system AMAZON_LINUX_2 \
+    --server-sdk-version 5.1.1 \
+    --build-root ./ServerBuild/ \
+    --name "Java Game Server Example" \
+    --build-version $buildversion \
+    --region $region
