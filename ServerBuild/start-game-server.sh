5973a63712a004f9e3bcb13d766b2b4f3059a8cc
dahuahua <38651850@qq.com>
2023-09-15 14:21:09 +0800

    add upload command

---
+++ ServerBuild/start-game-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar gamelift-java-game-server-demo-1.0-SNAPSHOT-all.jar
---
+++ build-and-deploy-game-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./gradlew clean build
+rm -rf ./ServerBuild/gamelift-java-game-server-demo-1.0-SNAPSHOT-all.jar
+cp ./build/libs/gamelift-java-game-server-demo-1.0-SNAPSHOT-all.jar ./ServerBuild/
+
+region=us-east-1
+echo "Deploying build to GameLift in $region"
+buildversion=$(date +%Y-%m-%d.%H:%M:%S)
+aws gamelift upload-build --operating-system AMAZON_LINUX_2 --build-root ./ServerBuild/ --name "Java Game Server Example" --build-version $buildversion --region $region
