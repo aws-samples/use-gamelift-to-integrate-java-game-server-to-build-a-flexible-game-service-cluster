8bc234fe6c1ba352d78ce706d4c8351f0e3bbcbc
dahuahua <38651850@qq.com>
2023-09-23 13:09:16 +0800

    add fluent-bit configuration

--- java-game-server/ServerBuild/install.sh
+++ java-game-server/ServerBuild/install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "install dependencies here"
+curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
+sudo mv ./fluent-bit.conf /etc/fluent-bit/fluent-bit.conf
+sudo systemctl start fluent-bit
