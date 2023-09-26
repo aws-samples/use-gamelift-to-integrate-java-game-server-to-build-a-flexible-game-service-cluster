544506b8aa0289d4501dd43b462340f14600c532
dahuahua <38651850@qq.com>
2023-09-26 17:56:10 +0800

    setup log file

--- java-game-server/ServerBuild/install.sh
+++ java-game-server/ServerBuild/install.sh
@@ -2,4 +2,18 @@
 echo "install dependencies here"
 curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
 sudo mv ./fluent-bit.conf /etc/fluent-bit/fluent-bit.conf
+## we need to create log folder at first, because if not the fluent bit will not be started success.
+logs_path="./logs"
+if [ ! -d "$logs_path" ]; then
+    echo "log folder not exists..."
+    mkdir -p "$logs_path"
+    if [ $? -eq 0 ]; then
+        echo "log folder create success."
+    else
+        echo "log folder create failed."
+        exit 1
+    fi
+else
+    echo "log folder exists。"
+fi
 sudo systemctl start fluent-bit
--- java-game-server/ServerBuild/start-game-server.sh
+++ java-game-server/ServerBuild/start-game-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar java-game-server-1.0-SNAPSHOT-all.jar
+java -jar java-game-server-1.0-SNAPSHOT-all.jar > mylogfile_$$$.log
